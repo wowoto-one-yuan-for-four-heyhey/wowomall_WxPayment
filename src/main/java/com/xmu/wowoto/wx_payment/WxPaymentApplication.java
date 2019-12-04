d5a0e0ac63b48d8b678177f835adc2384cfcef68
medalwill <42199368+medalwill@users.noreply.github.com>
2019-12-04 15:55:33 +0800

    1
    1

---
+++ src/main/java/com/xmu/wowoto/wx_payment/WxPaymentApplication.java
@@ -0,0 +1,13 @@
+package com.xmu.wowoto.wx_payment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WxPaymentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WxPaymentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xmu/wowoto/wx_payment/WxPaymentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xmu.wowoto.wx_payment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WxPaymentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
