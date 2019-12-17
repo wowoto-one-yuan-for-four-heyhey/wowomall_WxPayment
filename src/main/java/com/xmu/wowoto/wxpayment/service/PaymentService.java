227c4b3a1f3feaaea2f88619279b9994b3be2fd4
medalwill <42199368+medalwill@users.noreply.github.com>
2019-12-17 20:47:03 +0800

    1

--- src/main/java/com/xmu/wowoto/wxpayment/service/PaymentService.java
+++ src/main/java/com/xmu/wowoto/wxpayment/service/PaymentService.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 
-@FeignClient("wowoto-payment")
+@FeignClient("paymentService")
 public interface PaymentService {
 
     /**
