24cdc3ffaaa3998010cc3a1b101267abcf2eb92f
muluoyanran <muluoyanran@users.noreply.github.com>
2019-12-17 23:26:00 +0800

    wxPayment

--- src/main/java/com/xmu/wowoto/wxpayment/util/PrepayIdUtil.java
+++ src/main/java/com/xmu/wowoto/wxpayment/util/PrepayIdUtil.java
@@ -2,10 +2,18 @@ package com.xmu.wowoto.wxpayment.util;
 
 import com.xmu.wowoto.wxpayment.domain.WxPayment;
 
+import java.util.Random;
+
 public class PrepayIdUtil {
 
     static public String encodePrepayId(WxPayment wxPayment){
-        return wxPayment.getPayment().getId().toString();
+        Integer length = 8;
+        String lendata = "";
+        Random r = new Random();
+        for(int i = 0 ;i < length ;i++) {
+            lendata += String.valueOf((char)(new Random().nextInt(26)+65));
+        }
+        return lendata;
     }
 
 }
